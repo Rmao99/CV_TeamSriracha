367c1f1d9cd41df775bb94a9a103e91c651631f9
Richard Mao <richard.mao.123@gmail.com>
2016-11-11 19:42:17 -0800

    Added video device code to start camera testing tomorrow

---
+++ src/VideoDevice.hpp
@@ -0,0 +1,22 @@
+#ifndef VIDEO_DEVICE_H
+#define VIDEO_DEVICE_H
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+class VideoDevice
+{
+	public: 
+		VideoDevice();
+		void startCapture(int id);
+		Mat getImage();
+	private:
+		void takeImage();
+
+		VideoCapture camera;
+		Mat image;
+		int isFinished;
+		int isReady;
+		bool isInitialized;
+
+}
