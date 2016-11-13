e14a10791c9bb261792e24d5f98484ba3f00b18a
Richard Mao <richard.mao.123@gmail.com>
2016-11-12 16:46:37 -0800

    Added more functionality, fix the camera testing code and integrate the other parts

---
+++ build/src/CMakeFiles/cv_camera_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cv_camera_test.dir/cv_camera_test.cpp.o"
+  "CMakeFiles/cv_camera_test.dir/VideoDevice.cpp.o"
+  "cv_camera_test.pdb"
+  "cv_camera_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cv_camera_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/build/CMakeFiles/cv_camera_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cv_camera_test.dir/cv_camera_test.o"
+  "CMakeFiles/cv_camera_test.dir/VideoDevice.o"
+  "CMakeFiles/cv_camera_test.dir/AppConfig.o"
+  "CMakeFiles/cv_camera_test.dir/CmdLineInterface.o"
+  "cv_camera_test.pdb"
+  "cv_camera_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cv_camera_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
